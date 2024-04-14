fd260ddc70f523c39a30c9bbfb18fdf386450c54
ryo_kihara <rio.kid.no.13@gmail.com>
2024-04-14 02:03:25 -0700

    Add reboot functionality to AntMinerCGI queries

---
+++ internal/application/miner/ant_miner_cgi/queries/reboot.go
@@ -0,0 +1,50 @@
+package queries
+
+import (
+	"fmt"
+	"foxhound/pkg/http_auth"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+// cgi-bin/reboot.cgi: Reboot the miner.
+
+func AntMinerCGIReboot(username, password, ipAddress string) error {
+
+	t := http_auth.NewTransport(username, password)
+
+	newRequest, err := http.NewRequest("GET", fmt.Sprintf("http://%s/cgi-bin/reboot.cgi", ipAddress), nil)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error":      err,
+			"newRequest": newRequest,
+		}).Debug("Error creating new request")
+		return err
+	}
+
+	resp, err := t.RoundTrip(newRequest)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+			"resp":  resp,
+		}).Debug("Error creating new request")
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+			"body":  body,
+		}).Debug("Error reading response body")
+		return err
+	}
+
+	log.Println("RESULT OF REBOOT", string(body))
+
+	return nil
+}
