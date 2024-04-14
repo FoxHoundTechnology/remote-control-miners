674b21d308e99a3035ca6b53786dbe48ee69e4f9
ryo_kihara <rio.kid.no.13@gmail.com>
2024-04-14 01:59:24 -0700

    Refactor domain values and remove unused code

--- internal/application/miner/domain/values.go
+++ internal/application/miner/domain/values.go
@@ -1,9 +1,12 @@
-package miner
+package domain
 
 import (
+	"database/sql/driver"
+	"errors"
 	"time"
 )
 
+// OPTION: segregate the domain values with value/scan methods into their respective files as it grows
 type Mode int
 
 const (
@@ -29,16 +32,10 @@ type Config struct {
 }
 
 type Stats struct {
-	HashRate    float64 // in GH/s
-	RateIdeal   float64 // in GH/s
-	Uptime      int     // in seconds
-	Location    string
-	FanSpeed    int
+	HashRate    float64   // in GH/s
+	RateIdeal   float64   // in GH/s
+	Uptime      int       // in seconds
 	LastUpdated time.Time // UTC timestamp
-}
-
-type Pools struct {
-	Pools []Pool
 }
 
 type Pool struct {
@@ -51,20 +48,100 @@ type Fleet struct {
 	Miners []Miner
 }
 
-type Temperature struct {
-	TemperatureSensors []TemperatureSensor
-}
-
 type TemperatureSensor struct {
-	Name string
-	Temp float64
-}
-
-type Fan struct {
-	FanSensors []FanSensor
+	Name    string
+	TempPcb []int
 }
 
 type FanSensor struct {
 	Name  string
 	Speed int
+}
+
+type Log struct {
+	Name      string
+	EventType EventType
+	Timestamp time.Time
+}
+
+type EventType int
+
+const (
+	Operational  = iota // 0
+	SystemIssue         // 1
+	UserActivity        // 2
+)
+
+// Value/Scan methods for Mode
+func (m Mode) Value() (driver.Value, error) {
+	// Convert the Mode to its string representation
+	switch m {
+	case NormalMode:
+		return "normal", nil
+	case SleepMode:
+		return "sleep", nil
+	case LowPowerMode:
+		return "lowpower", nil
+	default:
+		return nil, errors.New("invalid Mode value")
+	}
+}
+
+func (m *Mode) Scan(value interface{}) error {
+	s, ok := value.(string)
+	if !ok {
+		return errors.New("conversion error: failed to convert the interface to string")
+	}
+	switch s {
+	case "normal":
+		*m = NormalMode
+	case "sleep":
+		*m = SleepMode
+	case "lowpower":
+		*m = LowPowerMode
+	default:
+		return errors.New("invalid Mode string")
+	}
+	return nil
+}
+
+// Value converts the Status to a string that can be stored in the database.
+func (s Status) Value() (driver.Value, error) {
+	switch s {
+	case Online:
+		return "online", nil
+	case Offline:
+		return "offline", nil
+	case Disabled:
+		return "disabled", nil
+	case Error:
+		return "error", nil
+	case Warning:
+		return "warning", nil
+	default:
+		return nil, errors.New("invalid Status value")
+	}
+}
+
+// Scan reads a string from the database and converts it to a Status.
+func (s *Status) Scan(value interface{}) error {
+	str, ok := value.(string)
+	if !ok {
+		return errors.New("conversion error: failed to convert the interface to string")
+	}
+	switch str {
+	case "online":
+		*s = Online
+	case "offline":
+		*s = Offline
+	case "disabled":
+		*s = Disabled
+	case "error":
+		*s = Error
+	case "warning":
+		*s = Warning
+	default:
+		return errors.New("invalid Status string")
+	}
+	return nil
 }
